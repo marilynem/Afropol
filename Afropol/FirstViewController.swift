3b5394d97a4474f524978aba61e251e768305039
Vanessa Komesha Mabambu <Vanessa@Vanessas-MacBook-Air.local>
2017-12-01 19:22:33 +0100

    Initial Commit

---
+++ Afropol/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Afropol
+//
+//  Created by Vanessa Komesha Mabambu on 2017-12-01.
+//  Copyright © 2017 Afropolitan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Afropol/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Afropol
+//
+//  Created by Vanessa Komesha Mabambu on 2017-12-01.
+//  Copyright © 2017 Afropolitan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
